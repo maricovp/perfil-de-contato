146664f223b253b6b0b40e91f04dcd9888911c9a
maricovp <marcos2015vinicuis@gmail.com>
2024-08-07 11:16:45 -0400

    complete page, so falta arrumar a estetica

--- scripts.js
+++ scripts.js
@@ -4,3 +4,17 @@ document
     event.preventDefault();
     alert("Formulário enviado com sucesso!");
   });
+ function handleSubmit(event) {
+            event.preventDefault(); // Prevent the form from submitting normally
+
+            // Perform any desired form processing here (e.g., sending data to the server)
+
+            // Clear the form
+            document.getElementById('myForm').reset();
+
+            // Optionally, provide feedback to the user
+            alert('Form submitted successfully!');
+
+            return false; // Prevent the form from submitting normally
+        }
+    
